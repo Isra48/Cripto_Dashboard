d1a3c33bf0ebe733c2ced85480577a13e021b2b1
Isra48 <israelalbarran48@gmail.com>
2025-06-03 16:17:43 -0600

    :sparkles: (api) connected fetchTopCryptos to real API

--- src/services/cryptoServices.ts
+++ src/services/cryptoServices.ts
@@ -1,16 +1,23 @@
-
 import type { CryptoCoin } from '../interfaces/crypto.interface'
-import { topCryptos } from '../mocks/topCryptos.mock'
+
 
 export const fetchTopCryptos = async (): Promise<CryptoCoin[]> => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const shouldFail = Math.random() < 0.1 
-      if (shouldFail) {
-        reject(new Error('No se pudo obtener los datos de las criptomonedas'))
-      } else {
-        resolve(topCryptos)
-      }
-    }, 1500)
-  })
+  const response = await fetch(
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en&x_cg_demo_api_key=CG-fVDb1nKqs5855FB7eqCpP94g`,
+    {
+      headers: {
+        Accept: 'application/json',
+        'User-Agent': 'CryptoDashboard/1.0.0',
+      },
+      cache: 'no-store',
+    }
+  )
+
+  if (!response.ok) {
+    throw new Error(`API responded with status: ${response.status}`)
+  }
+
+  const data = await response.json()
+
+  return data as CryptoCoin[]
 }
